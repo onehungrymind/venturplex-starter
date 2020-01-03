285845c2225bbdb26bf45287ecbef7487635961b
Victor Avila <victor.avila@venturplex.com>
2020-01-03 16:04:04 -0700

    feat: setup material and global styles

--- libs/material/README.md
+++ libs/material/README.md
@@ -1,3 +1,7 @@
 # Welcome To The Material Library
 
 This library was created to better control UI peices used within an app. We generally use Angular Material in all of our Angular apps. Creating this lib just makes it easier to decide what UI modules we use throughout all apps.
+
+## Enabling Gesture Recognition
+
+When generating a new application and desire mobile gesture recognition, please import `hammerjs` in the apps `main.ts` file.
--- libs/material/src/lib/material.module.ts
+++ libs/material/src/lib/material.module.ts
@@ -1,7 +1,72 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSnackBarModule,
+  MatTableModule,
+  MatToolbarModule,
+  MatButtonToggleModule,
+  // Imports not needing to be added to MODULES
+  MatChipsDefaultOptions,
+  MatFormFieldDefaultOptions,
+  MAT_CHIPS_DEFAULT_OPTIONS,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS
+} from '@angular/material';
+
+const MODULES = [
+  BrowserAnimationsModule,
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSnackBarModule,
+  MatTableModule,
+  MatToolbarModule,
+  MatButtonToggleModule
+];
+
+// Set Default Material Options.
+const matChipsOptions: MatChipsDefaultOptions = {
+  separatorKeyCodes: [ENTER, COMMA]
+};
+
+const matFormFieldOptions: MatFormFieldDefaultOptions = {
+  appearance: 'outline'
+};
+
 
 @NgModule({
-  imports: [CommonModule]
+  imports: MODULES,
+  exports: MODULES,
+  providers: [
+    {
+      provide: MAT_CHIPS_DEFAULT_OPTIONS,
+      useValue: matChipsOptions
+    },
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: matFormFieldOptions
+    }
+  ],
 })
 export class MaterialModule {}
