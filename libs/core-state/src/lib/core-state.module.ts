889b67ecedccb7331913377362ea40dfa452fda5
Victor Avila <victor.avila@venturplex.com>
2020-01-03 16:15:06 -0700

    feat: setup ngrx

--- libs/core-state/src/lib/core-state.module.ts
+++ libs/core-state/src/lib/core-state.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { DataPersistence } from '@nrwl/angular';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { RootStoreConfig, StoreModule } from '@ngrx/store';
+
+import { reducers } from '.';
+
+const storeConfig: RootStoreConfig<any> = {
+  runtimeChecks: {
+    strictActionImmutability: true,
+    strictStateImmutability: true
+  }
+};
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [
+    StoreModule.forRoot(reducers, storeConfig),
+    EffectsModule.forRoot([
+
+    ]),
+    StoreDevtoolsModule.instrument({ name: 'VenturPlex Starter Store' })
+  ],
+  providers: [DataPersistence]
 })
 export class CoreStateModule {}
---
+++ libs/core-state/src/lib/index.ts
@@ -0,0 +1,14 @@
+import { ActionReducerMap } from '@ngrx/store';
+
+// tslint:disable-next-line: no-empty-interface
+export interface AppState {
+
+}
+
+export const reducers: ActionReducerMap<AppState> = {
+
+}
+
+//---------------------------------------
+// Aggregated Data Selectors
+//---------------------------------------
