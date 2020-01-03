78a93d25bf2103ee8ad83d78dd7339c0deeaaeca
Victor Avila <victor.avila@venturplex.com>
2020-01-03 16:21:22 -0700

    feat: setup apollo

--- libs/core-data/src/lib/core-data.module.ts
+++ libs/core-data/src/lib/core-data.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
+import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const uri = 'http://0.0.0.0:8080/v1/graphql';
+
+export function createApollo(httpLink: HttpLink) {
+  return {
+    link: httpLink.create({ uri }),
+    cache: new InMemoryCache()
+  };
+}
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [HttpClientModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink]
+    }
+  ],
+  exports: [ApolloModule, HttpLinkModule]
 })
 export class CoreDataModule {}
