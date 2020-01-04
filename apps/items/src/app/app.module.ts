db962dbd532941c5d8193659c3b3df7f66974cb6
Victor Avila <victor.avila@venturplex.com>
2020-01-03 17:44:27 -0700

    setup: app with modules and styles

--- apps/items/src/app/app.module.ts
+++ apps/items/src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CoreDataModule } from '@workspace/core-data';
+import { CoreStateModule } from '@workspace/core-state';
+import { MaterialModule } from '@workspace/material';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    CoreDataModule,
+    CoreStateModule,
+    MaterialModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
