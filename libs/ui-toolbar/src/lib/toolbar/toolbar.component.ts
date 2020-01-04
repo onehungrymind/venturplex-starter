b870b2b34c7678f67622bd22c069516389ff08c1
Victor Avila <victor.avila@venturplex.com>
2020-01-03 18:20:06 -0700

    create: toolbar ui

--- libs/ui-toolbar/src/lib/toolbar/toolbar.component.ts
+++ libs/ui-toolbar/src/lib/toolbar/toolbar.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'workspace-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ToolbarComponent {
+  @Input() title: string;
 }
--- libs/ui-toolbar/src/lib/ui-toolbar.module.ts
+++ libs/ui-toolbar/src/lib/ui-toolbar.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MaterialModule } from '@workspace/material';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MaterialModule
+  ],
   declarations: [ToolbarComponent],
   exports: [ToolbarComponent]
 })
